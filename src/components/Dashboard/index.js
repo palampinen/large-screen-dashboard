f498dbf8408d2ce2e16a85160e1182c554ee9e0b
Pasi Lampinen <pasi.lampinen@futurice.com>
2016-11-17 19:24:55 +0200

    Dashboard scrolling

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -6,12 +6,16 @@ import './dashboard.css';
 class dashboard extends Component {
 
   render() {
-
   	const { widgets, selectItem } = this.props;
     return (
-      <div className="dashboard">
+      <div id="dashboard" className="dashboard">
         {widgets.map((item, index) => (
-          <Widget key={index} config={item} type={item.type} selectItem={selectItem} />
+          <Widget
+            key={index}
+            config={item.config || {}}
+            size={item.size}
+            type={item.type}
+            selectItem={selectItem} />
         ))}
       </div>
     );
